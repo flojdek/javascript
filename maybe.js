5b5ed09b53cd929f1a9633fcb681aa1b9cebe66f
Tomasz Kupczyk <kupczyk.tomasz@gmail.com>
2012-11-08 23:00:51 +0000

    Much nicer now.

--- maybe.js
+++ maybe.js
@@ -5,34 +5,49 @@ let getC = function (b) { return b; }
 let unit = function (x) { return x; };
 
 let bind = function (m, f) {
-    let ret = m;
-    if (ret) {
-        return f(ret);
+    let result = m;
+    if (result) {
+        return f(result);
     }
     else {
         return null;
     }
 };
 
-print(bind(getA(1),
-           function (a) {
-               return bind(getB(a),
-                           function (b) {
-                               return bind(getC(b),
-                                           function (c) {
-                                               return unit(c);
+let pipe = function (m, fs) {
+    let result = m;
+    for (let i = 0; i < fs.length; ++i) {
+        result = bind(result, fs[i]);
+    }
+    return result;
+};
+
+print(bind(unit(666),
+           function (x) {
+               return bind(getA(x),
+                           function (a) {
+                               return bind(getB(a),
+                                           function (b) {
+                                               return bind(getC(b),
+                                                           function (c) {
+                                                               return unit(c);
+                                                           })
                                            })
                            })
-           })
+            })
      );
+
+print(bind(bind(bind(unit(666), getA), getB), getC));
+
+print(pipe(unit(666), [getA, getB, getC]));
 
 let obj = {
     property1: {
-        property2: 1
+        property2: 666
     }
 };
 
-print(bind(obj,
+print(bind(unit(obj),
            function (a) {
                return bind(a.property1,
                            function (b) {
